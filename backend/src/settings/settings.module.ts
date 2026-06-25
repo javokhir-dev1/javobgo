e2f39a173ba1ddb1272e6c810db634e86fdd9b47
Javokhir <javokhir.dev1@gmail.com>
2026-06-25 16:12:28 +0300

    login fix 2

--- backend/src/settings/settings.module.ts
+++ backend/src/settings/settings.module.ts
@@ -3,9 +3,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Settings } from './entities/settings.entity';
 import { SettingsService } from './settings.service';
 import { SettingsController } from './settings.controller';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Settings])],
+  imports: [
+    TypeOrmModule.forFeature([Settings]),
+    AuthModule,
+  ],
   providers: [SettingsService],
   controllers: [SettingsController],
   exports: [SettingsService],
